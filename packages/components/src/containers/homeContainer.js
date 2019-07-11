e55560990baa544a3f7e0775bdc20140a7a741e8
ReallyLiri <liris@diagnosticrobotics.com>
2019-07-11 18:20:37 +0300

    Attempt on scrollable

--- packages/components/src/containers/homeContainer.js
+++ packages/components/src/containers/homeContainer.js
@@ -6,6 +6,7 @@ import connectComponent from "../redux/connect";
 import LocalStorage from "../infra/local-storage"
 import MatchingService from "../services/matchingService";
 import { uniteStyle } from "../theme/styleSheets";
+import CollapsingFooter from "../components/collapsing-footer";
 import { ROUTES } from "../routes";
 import * as chatActions from "../redux/actions/chatActions";
 
@@ -41,6 +42,23 @@ export class HomeContainer extends BaseContainerComponent {
     this.props.navigationActions.pushNavigation(ROUTES.CHAT);
   }
 
+  _renderView = (collapse) => {
+    return (
+      <View>
+        <Text>Toggle {collapse ? 'on' : 'off'}</Text>
+      </View>
+    )
+  }
+
+  _renderCollapseView = (collapse) => {
+    return(
+      <View>
+        <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
+      </View>
+    )
+  }
+
+
   render() {
     if (!this.state.ready) {
       return this.loadingPlaceholder();
@@ -48,6 +66,10 @@ export class HomeContainer extends BaseContainerComponent {
     console.error(this.state.matches);
     return (
       <View style={{width: 200}}>
+        <CollapsingFooter
+          renderView={this._renderView}
+          renderCollapseView={this._renderCollapseView}
+        />
         <Text>Matches for {this.props.userState.user.username}:<br/><br/></Text>
         {
           this.state.matches && this.state.matches.length ?
